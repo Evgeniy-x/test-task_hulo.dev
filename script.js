6fbee125f57d8d4123b4bebe43657118e15bf718
Evgeniy <yaremenko.x@gmail.com>
2024-02-23 22:00:34 +0200

    add SimpleLightbox

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const clientToken = "5566eb86a7a59ccf4afb33564a1d3e57";
+  // const clientSecret = "YOUR_CLIENT_SECRET";
 
   const videoIds = [
     "824804225",
@@ -11,8 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
     "824804225",
     "824804225",
   ];
-
-  const sliderContainer = document.querySelector(".glide");
 
   function getVimeoData(videoId) {
     return new Promise((resolve, reject) => {
@@ -28,7 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Function to create Vimeo video slidesvideo-slide
+  const sliderContainer = document.querySelector(".glide");
+
+  // Function to create Vimeo video slides
   async function createVideoSlides() {
     for (const videoId of videoIds) {
       const videoData = await getVimeoData(videoId);
@@ -36,51 +37,52 @@ document.addEventListener("DOMContentLoaded", function () {
       const videoEmbedUrl = `https://player.vimeo.com/video/${videoId}`;
       const slide = `
         <li class="glide__slide">
-          <a class="fancybox" data-src="${videoEmbedUrl}" data-options='{"speed": 700, "showCloseButton": true}'>
-            <img src="${videoThumbnailUrl}" alt="Video Thumbnail">
+          <a href="${videoEmbedUrl}" class="video-popup" data-vimeo-id="${videoId}">
+            <img src="${videoThumbnailUrl}" alt="Video thumbnail">
           </a>
         </li>
       `;
 
       sliderContainer.insertAdjacentHTML("beforeend", slide);
     }
-
-    //   Initialize Fancybox for opening videos in pop-up
-
-    // $(".fancybox").fancybox({
-    //   type: "iframe",
-    //   iframe: {
-    //     preload: false,
-    //   },
-    //   afterShow: function (instance, current) {
-    //     // Autoplay the video after the pop-up is shown
-    //     const iframe = current.$content.find("iframe");
-    //     const player = new Vimeo.Player(iframe[0]);
-    //     player.play();
-    //   },
-    // });
   }
-
-  // Call the function to create video slides
   createVideoSlides();
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const glide = new Glide(sliderContainer, {
-      type: "slider",
-      startAt: 0,
-      perView: 4,
-      focusAt: "center",
-      breakpoints: {
-        768: {
-          perView: 2,
-        },
+  // Чекаєм рендер сторінки, потім виконуєм:
+  new Glide(".glide", {
+    type: "slider",
+    startAt: 0,
+    perView: 4,
+    focusAt: "center",
+    breakpoints: {
+      768: {
+        perView: 2,
       },
-      pagination: {
-        el: ".glide__pagination",
-        clickable: true,
-      },
-    });
+    },
+  }).mount();
 
-    glide.mount();
+  const lightbox = new SimpleLightbox(".video-popup", {
+    nav: true,
+    navText: ["&larr;", "&rarr;"],
+    docClose: false, // Запобігає закриттю попапу при кліку поза ним
+    captionsData: "alt",
+    autoplay: true, // Автоматичне відтворення відео
+    autoplaySpeed: 500, // Швидкість автоматичного відтворення відео (в мілісекундах)
+    dots: true, // Пагінація з точок
+    slideEnd: function () {
+      // Викликається після переходу до нового слайду
+      // Тут ви можете вставити код для автоматичного відтворення відео, якщо це потрібно
+    },
   });
+
+  // Зупинка автоматичного відтворення при відкритті попапу
+  lightbox.on("show.simplelightbox", function () {
+    lightbox.autoplayStop();
+  });
+
+  // Відновлення автоматичного відтворення при закритті попапу
+  lightbox.on("close.simplelightbox", function () {
+    lightbox.autoplayStart();
+  });
+  // Call the function to create video slides
 });
